193ac24002821a44f874fe236e09355c9e6978eb
damian <damian.hr.rodriguez@gmail.com>
2019-05-11 21:17:27 -0300

    avance app casi terminada

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,8 +2,54 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Formulario from './Formulario';
+import Listado from './Listado';
+import {validarPresupuesto} from '../helper';
 
 class App extends Component {
+
+    state = {
+      presupuesto : '',
+      restante: '',
+      gastos: {} ,
+    }
+
+    componentDidMount() { //mejor un metodo para no cargar al didmount
+      this.obtenerPresupuesto();
+      
+    }
+
+    obtenerPresupuesto = () => {
+      let presupuesto = prompt('Cual es el presupuesto?');
+
+      let resultado = validarPresupuesto(presupuesto);
+      if(resultado) {
+        this.setState({
+          presupuesto: presupuesto,
+          restante : presupuesto
+        })
+      } else {
+        this.obtenerPresupuesto();
+      }
+    }
+
+   
+  //agregar un nuevo gasto al state
+  agregarGasto = gasto =>{
+    // tomar una copia del state actual
+    const gastos = {...this.state.gastos};
+  
+
+    //agregar al gasto al objeto del state
+    gastos[`gasto${Date.now()}`] = gasto;
+
+    console.log(gastos);
+
+    //ponerlo en el state
+    this.setState({
+      gastos
+    })
+
+  }
   render(){
         return (
           <div className="App container">
@@ -13,10 +59,14 @@ class App extends Component {
             <div className="contenido-principal contenido">
               <div className="row">
                 <div className="one-half column">
-                  <Formulario />
+                  <Formulario
+                    agregarGasto = {this.agregarGasto}
+                  />
               </div>
               <div className="one-half column">
-          
+                  <Listado
+                  gastos={this.state.gastos}
+                  />
               </div>
 
               </div>
--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,19 +1,40 @@
 import React , {Component} from 'react';
 
 class FormularioGasto extends Component{
+
+    //refs para campos del formulario
+
+    nombreGasto = React.createRef();
+    cantidadGasto = React.createRef();
+
+    crearGasto = (e) =>{
+        //prevenir el default
+        e.preventDefault();
+
+        //crear el objeto con los datos
+        const gasto = {
+            nombreGasto : this.nombreGasto.current.value,
+            cantidadGasto : this.cantidadGasto.current.value,
+        }
+        //agregarlo y enviarlo por props
+        this.props.agregarGasto(gasto);
+        //resetear el formulario (opcional)
+        e.currentTarget.reset();
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={this.crearGasto}>
                 <h2>Agrega tus gastos aqui</h2>
 
                 <div className="campo">
                     <label>Nombre Gasto</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                    <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
                 </div>
 
                 <div className="campo">
                     <label>Cantidad</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. 300" />
+                    <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
                 </div>
 
                 <input className="button-primary u-full-width" type="submit" 
